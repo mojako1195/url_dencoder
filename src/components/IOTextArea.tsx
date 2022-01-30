945a9064ede44cbc812ce9cad255559b10cfc974
mojako <magonote1195@gmail.com>
2022-01-30 21:40:52 +0900

    220130 textarea Component 作成

---
+++ src/components/IOTextArea.tsx
@@ -0,0 +1,82 @@
+import { ChangeEvent, useState } from "react";
+import styled from "styled-components";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+
+export const IOTextArea = () => {
+  const [beforeText, setBeforeText] = useState<string>("");
+  const [afterText, setAfterText] = useState<string>("");
+  const MySwal = withReactContent(Swal);
+
+  const onChangeBeforeText = (e: ChangeEvent<HTMLInputElement>) =>
+    setBeforeText(e.target.value);
+
+  const onClickCopyBtn = () => {
+    navigator.clipboard.writeText(afterText).then(
+      () => {
+        MySwal.fire({
+          title: <strong>コピーしました。</strong>,
+        });
+      },
+      (err) => {
+        MySwal.fire({
+          title: <strong>コピーできませんでした。</strong>,
+        });
+      }
+    );
+  };
+
+  const onClickConvBtn = () => {
+    setAfterText(beforeText);
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col sm={5}>
+          <Form.Group>
+            <Form.Label>
+              <TiTleStyle>変換前</TiTleStyle>
+            </Form.Label>
+            <Form.Control
+              as="textarea"
+              value={beforeText}
+              onChange={onChangeBeforeText}
+            />
+          </Form.Group>
+          <div className="mt-3" style={{ textAlign: "right" }}>
+            <Button variant="primary" onClick={onClickConvBtn}>
+              変換
+            </Button>
+          </div>
+        </Col>
+        <Col sm={2}>
+          <Paragraph />
+        </Col>
+        <Col sm={5}>
+          <Form.Group>
+            <Form.Label>
+              <TiTleStyle>変換後</TiTleStyle>
+            </Form.Label>
+            <Form.Control as="textarea" value={afterText} readOnly />
+          </Form.Group>
+          <div className="mt-3" style={{ textAlign: "right" }}>
+            <Button variant="primary" onClick={onClickCopyBtn}>
+              コピー
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+const TiTleStyle = styled.div`
+  font-weight: bold;
+  font-size: large;
+`;
+
+const Paragraph = styled.div`
+  margin-top: 10px;
+`;
