18fc05109bfd8d9eb0a956c046f9f69371d5f0b8
mojako <magonote1195@gmail.com>
2022-01-30 21:41:29 +0900

    220130 RadioButton Component作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { PageHeader } from "./components/PageHeader";
+import { IOTextArea } from "./components/IOTextArea";
+import { RadioButtonComponent } from "./components/RadioButtonComponent";
 
 export const App = () => {
   return (
     <>
-      <PageHeader></PageHeader>
+      <PageHeader />
+      <RadioButtonComponent />
+      <IOTextArea />
     </>
   );
 };
---
+++ src/components/RadioButtonComponent.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useState } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
+import styled from "styled-components";
+
+export const RadioButtonComponent = () => {
+  const [configFlg, setConfigFlg] = useState<string>("0");
+
+  const onChangeConfigFlg = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfigFlg(e.target.value);
+  };
+
+  return (
+    <>
+      <Paragraph />
+      <Container>
+        <Row>
+          <Col sm={12}>
+            <Form.Check
+              type="radio"
+              id="encodeBtn"
+              name="configBtn"
+              label="エンコード（あ → %E3%81%82）"
+              value="0"
+              checked={configFlg == "0"}
+              onChange={onChangeConfigFlg}
+            />
+          </Col>
+        </Row>
+        <Paragraph />
+        <Row>
+          <Col sm={12}>
+            <Form.Check
+              type="radio"
+              id="decodeBtn"
+              name="configBtn"
+              label="デコード（%E3%81%82 → あ）"
+              value="1"
+              checked={configFlg == "1"}
+              onChange={onChangeConfigFlg}
+            />
+          </Col>
+        </Row>
+      </Container>
+      <Paragraph />
+    </>
+  );
+};
+
+const Paragraph = styled.div`
+  margin-top: 10px;
+`;
